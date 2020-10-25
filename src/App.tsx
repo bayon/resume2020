cb29e109c6628f4e56ecc341ce5a7a4ac878b5bd
Bayon Forte <bayon@forteworks.com>
2020-10-25 17:19:00 -0400

    simple baked in api call.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // BUILT IN API CALL: ========================================
+  // Running App on Local Host my machine, does in fact request data successfully. 
+  // The question now is, if it is deployed on digital ocean app...will it be able to access. ? 
+  useEffect(()=>{
+    fetch(
+      'http://134.122.29.54:3000/messages',
+      {
+        method: "GET",
+        
+      }
+    ).then( res => res.json())
+    .then(response => {
+      console.log("response data:",response)
+      
+    })
+    .catch(error => console.log(error))
+  })
+  //=============================================================
   return (
     <div className="App">
       <header className="App-header">
